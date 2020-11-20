bbbf76b5b85626f7239e07dbfc6faa35acff1fab
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2020-11-20 12:44:50 -0600

    rewired, redux-saga, router-dom, styled-components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components"; // or import styled from '@emotion/styled'
+import Layout, {
+  Root,
+  getHeader,
+  getDrawerSidebar,
+  getSidebarTrigger,
+  getSidebarContent,
+  getCollapseBtn,
+  getContent,
+  getFooter } from "@mui-treasury/layout";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-function App() {
+
+const Header = getHeader(styled)
+const DrawerSidebar = getDrawerSidebar(styled)
+const SidebarTrigger = getSidebarTrigger(styled)
+const SidebarContent = getSidebarContent(styled)
+const CollapseBtn = getCollapseBtn(styled)
+const Content = getContent(styled)
+const Footer = getFooter(styled)
+
+const scheme = Layout();
+
+scheme.configureHeader((builder) => {
+  builder
+    .create("whatever_id")
+    .registerConfig("xs", {
+      position: "sticky",
+    })
+    .registerConfig("md", {
+      position: "relative", // won't stick to top when scroll down
+    });
+});
+
+scheme.configureEdgeSidebar((builder) => {
+  builder
+    .create("unique_id", { anchor: "left" })
+    .registerTemporaryConfig("xs", {
+      anchor: "left",
+      width: "auto", // 'auto' is only valid for temporary variant
+    })
+    .registerPermanentConfig("md", {
+      width: 256, // px, (%, rem, em is compatible)
+      collapsible: true,
+      collapsedWidth: 64,
+    });
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root scheme={scheme}>
+      <CssBaseline />
+      <Header>
+        <Toolbar>
+          <SidebarTrigger sidebarId="unique_id" />
+          Header
+        </Toolbar>
+      </Header>
+      <DrawerSidebar sidebarId="unique_id">
+        <CollapseBtn />
+        <SidebarContent>
+        </SidebarContent>
+      </DrawerSidebar>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Root>
   );
-}
+};
 
 export default App;
