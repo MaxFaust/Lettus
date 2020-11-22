a070702bd02f36c610aa84e90fbd8267cade4d97
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2020-11-22 10:57:14 -0600

    set up routing through Nav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Root } from "@mui-treasury/layout";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Route, Switch } from "react-router-dom";
 
 import Nav from '../Nav/Nav';
+import Home from '../Home/Home'
 
 class App extends Component {
   render() {
     return (
     <Root>
+    <CssBaseline />
       <Nav/>
+      <Switch>
+        <Route exact from="/" render={props => <Home {...props} />} />
+        {/* <Route exact path="/contact" render={props => <Contact {...props} />} />
+        <Route exact path="/about" render={props => <About {...props} />} /> */}
+      </Switch>
     </Root>
   );
 };
